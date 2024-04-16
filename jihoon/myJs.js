51381bb6f3b0562285075b77c98cbcabde24118b
npureaun <98468118+npureaun@users.noreply.github.com>
2024-04-16 16:46:56 +0900

    새로고침시에 화면 index백업

--- jihoon/myJs.js
+++ jihoon/myJs.js
@@ -1,3 +1,6 @@
+////////////////////////////////////////////////////////////////////////////
+//동적화면비 관련 기능//
+///////////////////
 let resizeTimer; // 타이머를 저장할 변수
 window.addEventListener('resize', function () {
   // 이미 예약된 타이머가 있다면 취소.
@@ -7,63 +10,66 @@ window.addEventListener('resize', function () {
     window.location.reload();
   }, 250);
 });
+////////////////////////////////////////////////////////////////////////////
 
-// sessionStorage.js
-function getItem(key) {
+
+////////////////////////////////////////////////////////////////////////////
+//현재 슬라이드 백업 기능//
+/////////////////////
+function getItem(key) {//새로고침시 임시JSON에 index값 받아옴
   const value = sessionStorage.getItem(key);
-
   if (key === 'index') return value === null ? 0 : JSON.parse(value);
-  else return value === null ? 0 : JSON.parse(value);
 }
 
-function setItem(key, value) {
+function setItem(key, value) {//버튼클릭 시에 임시JSON에 값 저장
   if (value === null || value === undefined) return;
-
   const toJson = JSON.stringify(value);
-
   sessionStorage.setItem(key, toJson);
 }
+////////////////////////////////////////////////////////////////////////////
 
-//export { getItem, setItem };
 
-/*
-  div사이즈 동적으로 구하기
-*/
+////////////////////////////////////////////////////////////////////////////
+//div사이즈 동적으로 세팅하는 기능//
+///////////////////////////
 const outer = document.querySelector('.outer');
 const innerList = document.querySelector('.inner-list');
 const inners = document.querySelectorAll('.inner');
 let currentIndex = getItem('index'); // 현재 슬라이드 화면 인덱스
 
-if (window.innerHeight < 550) {
+if (window.innerHeight < 550) {//화면크기에 따른 버튼 크기 재할당
   document.getElementById('btnlist').style.top = '10px';
   document.getElementById('btnL').style.fontSize = '20px';
   document.getElementById('btnR').style.fontSize = '20px';
 }
-//let width=(outer.clientWidth<400)?outer.clientWidth : outer.clientWidth+20;
+
 let width = outer.clientWidth;
 inners.forEach((inner) => {
   inner.style.width = `${width}px`; // inner의 width를 모두 outer의 width로 만들기
 })
 
-
 innerList.style.width = `${width * inners.length}px`; // innerList의 width를 inner의 width * inner의 개수로 만들기
-/*
-  버튼에 이벤트 등록하기
-*/
+innerList.style.marginLeft = `-${width * currentIndex}px`;//저장된 슬라이드 index로 초가화
+////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////////////////////////////
+//버튼 이벤트 관련 기능//
+////////////////////
 const buttonLeft = document.querySelector('.button-left');
 const buttonRight = document.querySelector('.button-right');
-innerList.style.marginLeft = `-${width * currentIndex}px`;
 
 buttonLeft.addEventListener('click', () => {
   currentIndex--;
-  setItem('index', currentIndex);
   currentIndex = currentIndex < 0 ? inners.length - 1 : currentIndex; // index값이 0보다 작아질 경우 마지막으로 이동
+  setItem('index', currentIndex);//알맞게 조정된 현재 슬라이드index를 임시 JSON에 저장
   innerList.style.marginLeft = `-${width * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
 });
 
 buttonRight.addEventListener('click', () => {
   currentIndex++;
-  setItem('index', currentIndex);
   currentIndex = currentIndex >= inners.length ? 0 : currentIndex; // index값이 inner의 총 개수보다 많아질 경우 처음으로 이동
+  setItem('index', currentIndex);//알맞게 조정된 현재 슬라이드index를 임시 JSON에 저장
   innerList.style.marginLeft = `-${width * currentIndex}px`; // index만큼 margin을 주어 옆으로 밀기
 });
+////////////////////////////////////////////////////////////////////////////
