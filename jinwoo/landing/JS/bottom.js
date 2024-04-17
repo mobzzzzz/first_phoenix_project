7ca14f053acb73b95e82231157d3a2c4b34dbff0
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-04-17 11:20:01 +0900

    1차 CSS파일, JS파일 정리 및 HTML 구조정리

--- jinwoo/landing/JS/bottom.js
+++
@@ -1,5 +0,0 @@
-// 첫번째 아코디언 부분 자동으로 접어주기
-setTimeout(function(){
-    $("#bottom-information-subtitle-button").trigger("click")
-},6000)
-
--- jinwoo/landing/JS/tmi.js
+++
@@ -1,3 +0,0 @@
-$("#bottom-right-tmi-user-component-del-btn").click(function(){
-    $("#tmi-user-component").css("display","none")
-});
