1b9b14b29eb3d2668d0e4748dc4a09ea9e96ebdc
npureaun <98468118+npureaun@users.noreply.github.com>
2024-04-17 21:17:51 +0900

    ㄹㅇㅎㅇㄹ

--- jihoon/mainJs.js
+++ jihoon/mainJs.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $("#accordionTarget").appendTo("#card-group");
     $("#initTarget").appendTo("#accordionTarget");
     $("#accordionGuestbook").appendTo("#initTarget");
     $("#accordionGuestbook").css("margin-top", "50px");
