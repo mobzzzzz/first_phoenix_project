529bf551875620268d9d4e6b40a24367cdfd76f1
npureaun <98468118+npureaun@users.noreply.github.com>
2024-04-17 18:01:40 +0900

    메인만들기

--- jihoon/mainJs.js
+++ jihoon/mainJs.js
@@ -1,12 +1,6 @@
-let resizeTimer; // 타이머를 저장할 변수
-window.addEventListener('resize', function () {
-  // 이미 예약된 타이머가 있다면 취소.
-  clearTimeout(resizeTimer);
-  // 사용자가 리사이즈를 멈춘 후 250ms 후에 새로고침.
-  resizeTimer = setTimeout(function () {
-    if(window,innerWidth < 550)
-    {
-        
-    }
-  }, 250);
+$(document).ready(function(){
+    $("#initTarget").appendTo("#accordionTarget");
+    $("#accordionGuestbook").appendTo("#initTarget");
+    $("#accordionGuestbook").css("margin-top", "50px");
+    $("#initGuestbook").appendTo("#accordionGuestbook");
 });
