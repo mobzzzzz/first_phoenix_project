5d172eea19256e610e4379bfcda81f9d6d5efc1c
Jaewon Noh <opqw987@gmail.com>
2024-04-18 16:19:51 +0900

    Merge Jihoon workspace (#2)
    * 일단 저장
    
    * 메인만들기
    
    * ㄹㅇㅎㅇㄹ
    
    * ㄹㅇㅎ
    
    * yjyjyjyj
    
    * ㄷㄷㄷㄷㄹ
    
    ㄷ
    
    * 등아퍼
    
    * 마지막 커밋
    
    * 찐찐최종
    
    ---------
    
    Co-authored-by: npureaun <98468118+npureaun@users.noreply.github.com>

---
+++ jihoon/mainJs.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#accordionTarget").appendTo("#card-group");
+    $("#initTarget").appendTo("#accordionTarget");
+    $("#accordionGuestbook").appendTo("#initTarget");
+    $("#accordionGuestbook").css("margin-top", "50px");
+    $("#initGuestbook").appendTo("#accordionGuestbook");
+});
+
