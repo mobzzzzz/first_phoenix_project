39ec3b3c4730a982ebc62956ddd762b692bd9558
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-04-18 15:31:47 +0900

    모달 수정

--- jinwoo/landing/JS/landing-nav.js
+++ jinwoo/landing/JS/landing-nav.js
@@ -10,8 +10,6 @@ $("#landing-nav-button-todo").click(function(){
 $("#landing-nav-button-post").click(function(){
     window.scroll({top:5000,behavior: "smooth"})
 })
-
-
 $("#landing-nav-button-scroll-up").click(function(){
     window.scroll({top:0,behavior: "smooth"})
 })
