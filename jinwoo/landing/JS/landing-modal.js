184c778d775f6514e933ec0a700c2d2f3873f8a6
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-04-18 10:46:24 +0900

    모달 적용 전

--- jinwoo/landing/JS/landing-modal.js
+++ jinwoo/landing/JS/landing-modal.js
@@ -1,16 +1,17 @@
-// 아이콘 누르면 모달창 띄우기
-$("#bottom-right-tmi-warning-icon").click(function(){
-    $("#tmi-modal-box").css("display","inline")
+
+$("#landing-todo-content-list-item-delete-button").click(function(){
+    $("#landing-modal-box").css("display","inline")
 });
 
-// 모달창에서 no 버튼 누를 시 모달 사라지기
-$("#tmi-modal-inner-button-no").click(function(){
-    $("#tmi-modal-box").css("display","none")
+$("#landing-modal-inner-button-no").click(function(){
+    $("#landing-modal-box").css("display","none")
 });
 
-// 모달창에서 yes 버튼 누를 시 TMI창 나오고,모달 사라지기
-$("#tmi-modal-inner-button-yes").click(function(){
-    $("#tmi-modal-box").css("display","none")
-    $("#tmi-user-component").css("display","inline")
+
+$("#landing-modal-inner-button-yes").click(function(){
+    $("#landing-modal-box").css("display","none")
+    $("#landing-user-component").css("display","inline")
 });
+// 
+
 
