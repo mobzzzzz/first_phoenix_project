7d8034f394d385d7459e118803db2bd236fb8966
Jaewon Noh <opqw987@gmail.com>
2024-04-19 12:51:45 +0900

    Config secrets 배포

--- script/config.js
+++ script/config.js
@@ -1,13 +1,31 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getFirestore, Timestamp, collection, doc, getDocs, setDoc, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import {
+  getFirestore,
+  Timestamp,
+  collection,
+  doc,
+  getDocs,
+  setDoc,
+  deleteDoc,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
+// const firebaseConfig = {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.FIREBASE_APP_ID
+// };
 
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyDz4wh7z369v-whnyYeWaI3quPab2hxFmA",
+  authDomain: "phoenix-first-project.firebaseapp.com",
+  projectId: "phoenix-first-project",
+  storageBucket: "phoenix-first-project.appspot.com",
+  messagingSenderId: "943608506043",
+  appId: "1:943608506043:web:c6dc699ceedf5090406b9e",
 };
 
 // Initialize Firebase
@@ -16,4 +34,14 @@ const firestoreDB = getFirestore(firebaseApp);
 
 // 모듈을 다른 모듈에서 사용할 수 있게 내보냄
 // export, import를 쓰는 문법은 ES6 에서 추가된 방법이라 ESM(ECMA Script Module) 이라 부른다.
-export { firebaseApp, firestoreDB, Timestamp, collection, doc, setDoc, getDocs, deleteDoc, updateDoc };
+export {
+  firebaseApp,
+  firestoreDB,
+  Timestamp,
+  collection,
+  doc,
+  setDoc,
+  getDocs,
+  deleteDoc,
+  updateDoc,
+};
